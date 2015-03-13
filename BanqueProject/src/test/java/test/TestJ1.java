647fa2468778f8ca2f2d3aa524ed99c8fd2862b3
AMhadhbi <mhadhbi.achref@gmail.com>
2015-03-13 04:34:13 +0100

    Test J2

--- BanqueProject/src/test/java/test/TestJ1.java
+++ BanqueProject/src/test/java/test/TestJ1.java
@@ -2,8 +2,14 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
 import org.junit.Test;
 import org.projects.banque.entities.Client;
+import org.projects.banque.entities.CompteCourant;
+import org.projects.banque.entities.CompteEpargne;
+import org.projects.banque.entities.Employe;
+import org.projects.banque.entities.Groupe;
 import org.projects.banque.metier.IBanqueMetier;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -17,6 +23,24 @@ public class TestJ1 {
 			IBanqueMetier metier = (IBanqueMetier) context.getBean("metier");
 
 			metier.addClient(new Client("C1", "AD1"));
+			metier.addClient(new Client("C2", "AD2"));
+
+			metier.addGroupe(new Groupe("G1"));
+			metier.addGroupe(new Groupe("G2"));
+
+			metier.addEmploye(new Employe("E1"), null);
+			metier.addEmploye(new Employe("E2"), 1L);
+			metier.addEmploye(new Employe("E3"), 1L);
+
+			metier.addEmployeToGroupe(1L, 1L);
+			metier.addEmployeToGroupe(2L, 2L);
+			metier.addEmployeToGroupe(3L, 2L);
+
+			metier.addCompte(new CompteCourant("CE1", new Date(), 9000, 4000),
+					1L, 2L);
+			metier.addCompte(new CompteEpargne("CE1", new Date(), 5900, 5.5),
+					2L, 2L);
+
 			assert (true);
 		} catch (Exception e) {
 			assertTrue(e.getMessage(), false);
