b6478619d841078c48a17df052f0e5918e8d32fc
AMhadhbi <mhadhbi.achref@gmail.com>
2015-03-13 16:49:04 +0100

    add Model to Controller

--- BanqueProject/src/main/java/org/projects/banque/controller/BanqueController.java
+++ BanqueProject/src/main/java/org/projects/banque/controller/BanqueController.java
@@ -1,23 +1,23 @@
 package org.projects.banque.controller;
 
 import org.projects.banque.metier.IBanqueMetier;
+import org.projects.banque.model.BanqueForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
-@Controller  
+@Controller
 public class BanqueController {
-	
+
 	@Autowired
 	private IBanqueMetier metier;
-	
-	@RequestMapping(value="/index")
-	public String index(){
-		
+
+	@RequestMapping(value = "/index")
+	public String index(Model model) {
+		model.addAttribute("banqueForm", new BanqueForm());
 		return "banque";
-		
+
 	}
-	
 
 }
--- BanqueProject/src/main/webapp/WEB-INF/views/banque.jsp
+++ BanqueProject/src/main/webapp/WEB-INF/views/banque.jsp
@@ -1,4 +1,5 @@
-
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="f"%>
 <!DOCTYPE html >
 <html>
 <head>
@@ -6,6 +7,8 @@
 <title>Banque</title>
 </head>
 <body>
-Test
+<div>
+
+</div>
 </body>
 </html>
